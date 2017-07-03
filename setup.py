edd21762c41dd99b219386b9e9160e485cd08f78
Andrei Pradan <andrei.pradan@3pillarglobal.com>
2017-07-03 17:06:26 +0300

    updated setup

--- setup.py
+++ setup.py
@@ -2,13 +2,11 @@ from glob import glob
 from os.path import basename
 from os.path import splitext
 
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import find_packages, setup
 
 requires = [
     'xmltodict',
     'requests',
-    'python-dateutil',
 ]
 
 setup(
@@ -17,11 +15,16 @@ setup(
     description='Client library for interacting with Elemental Conductor',
     author='PBS Core Services Team',
     author_email='pbsi-team-core-services@pbs.org',
-    url='tba',
+    url='https://github.com/pbs/pymental',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     include_package_data=True,
     zip_safe=False,
     install_requires=requires,
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+    ],
+    keywords='pymental elemental conductor client',
 )
